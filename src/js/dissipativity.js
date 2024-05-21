f1fe5713111c5ca56dc3a5c6e956afa920d6f329
Lamotriz <107123179+Lamotriz@users.noreply.github.com>
2024-05-21 17:36:31 -0300

    Update dissipativity.js

--- src/js/dissipativity.js
+++ src/js/dissipativity.js
@@ -25,11 +25,3 @@ function addDissipativityContent(){
 }
 
 addDissipativityContent();
-
-// Initialize the map -> setView([latitude, longitude], zoom)
-var map = L.map('map').setView([-3.74544911119979, -38.57916624923715], 20);
-      
-// Add a tile layer (you can use other tile providers)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
