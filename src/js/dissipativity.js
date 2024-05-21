1c51dc84fe8607d2f6cd5de1087dadbad025f148
Lamotriz <107123179+Lamotriz@users.noreply.github.com>
2024-05-21 17:43:58 -0300

    Update dissipativity.js

--- src/js/dissipativity.js
+++ src/js/dissipativity.js
@@ -2,11 +2,6 @@
 function addDissipativityContent(){
     // Getting elements
     content = document.getElementById('content');
-
-    // Adding html code
-    content.innerHTML = 
-    
-        
     //"project_1": {
     //    "title": "Dissipativity based...",
     //    "last_update": "Last update on November 16th, 2023",
@@ -14,12 +9,11 @@ function addDissipativityContent(){
     //        "title": "Description...",
     //        "details": "Overview ..."
     //    },
-
-        
+    // Adding html code
+    content.innerHTML = 
     <h1> ${language.project_1.title} </h1>
-
     // <h2>${language.project_1.office_phone.title}</h2>
-    <p>${language.projet_1.description}</p>
+    <p>${language.projet_1.description.title}</p>
     <br>
   `;
 }
