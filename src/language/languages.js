4311e54b54d9bd59bccf9d23ae8806dd2fdf6f16
Lamotriz <107123179+Lamotriz@users.noreply.github.com>
2024-05-21 10:30:46 -0300

    Update languages.js

--- src/language/languages.js
+++ src/language/languages.js
@@ -30,6 +30,7 @@ var en = {
         "title": "NEACON",
         "introduction": "The 'Núcleo de Estudos em Automação Industrial e Controle Não-Linear' (Center for Studies in Industrial Automation and Non-Linear Control) of the Department of Electrical Engineering at the Federal University of Ceará has expertise in simulations and tests of industrial loads. The behavior is analyzed considering the application of automation and control techniques, for the operational optimization of the system or its energy efficiency."
     },
+    
     "publications":{
         "title": "Publications",
         "subtitle": "Most recent and relevant publications",
